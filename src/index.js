4cb101a34fbf849539e3d14aba05471274416304
Nate Abele <nate.abele@gmail.com>
2017-09-26 00:24:18 -0400

    Version bump
    Switch build to tsc
    
    Add TS definitions

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { default as Box } from './Box';
-import { default as Line } from './Line';
-import { default as Point } from './Point';
-import { default as Size } from './Size';
-
-export { Box, Line, Point, Size };
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Box } from './Box';
+export { default as Line } from './Line';
+export { default as Point } from './Point';
+export { default as Size } from './Size';
